b5fe59bec514e8d44558d47d9505352d820c140f
Remon Pel <remon@clearsite.nl>
2016-05-27 13:29:14 +0000

    up version
    git-svn-id: svn://svn.clearsite.nl/wp_plugins/wp-email-essentials/trunk@7013 ad331a6c-ddf7-4250-94be-11eacc3bd02f

--- wp-email-essentials.php
+++ wp-email-essentials.php
@@ -5,7 +5,7 @@
 	Plugin URI: https://bitbucket.org/rmpel/wp-email-essentials
 	Author: Remon Pel
 	Author URI: http://remonpel.nl
-	Version: 1.7.2
+	Version: 1.7.3
 	License: GPL2
 	Text Domain: Text Domain
 	Domain Path: Domain Path
