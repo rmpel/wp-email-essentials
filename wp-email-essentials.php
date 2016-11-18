627204b722d415d23d2971cfb8d6c3e1e2098171
Remon Pel <remon@clearsite.nl>
2016-11-18 14:57:49 +0000

    new version
    git-svn-id: svn://svn.clearsite.nl/wp_plugins/wp-email-essentials/trunk@7297 ad331a6c-ddf7-4250-94be-11eacc3bd02f

--- wp-email-essentials.php
+++ wp-email-essentials.php
@@ -5,28 +5,10 @@ Description: A must-have plugin for WordPress to get your outgoing e-mails strai
 Plugin URI: https://bitbucket.org/rmpel/wp-email-essentials
 Author: Remon Pel
 Author URI: http://remonpel.nl
-Version: 1.8.4
+Version: 1.8.5
 License: GPL2
 Text Domain: Text Domain
 Domain Path: Domain Path
-*/
-
-/*
-
-        Copyright (C) 2013-2016  Remon Pel  ik@remonpel.nl
-
-        This program is free software; you can redistribute it and/or modify
-        it under the terms of the GNU General Public License, version 2, as
-        published by the Free Software Foundation.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program; if not, write to the Free Software
-        Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class WP_Email_Essentials
