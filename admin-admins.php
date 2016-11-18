b9adaf2871b8c6df68f906f9d530df5f91c7e89e
Remon Pel <remon@clearsite.nl>
2016-11-18 14:55:49 +0000

    identify valid and invalid regular expressions, identify what unmatched subjects will be matched by current focus regexp
    git-svn-id: svn://svn.clearsite.nl/wp_plugins/wp-email-essentials/trunk@7296 ad331a6c-ddf7-4250-94be-11eacc3bd02f

--- admin-admins.php
+++ admin-admins.php
@@ -37,7 +37,7 @@ $admin = get_option('admin_email');
 			<?php $i = 0; $exps = get_option('mail_key_list', array()); foreach ($exps as $regexp => $key) { ?>
 				<tr>
 					<td>
-						<input type="text" name="settings[regexp][<?php print $i ?>][regexp]" value="<?php print esc_attr($regexp); ?>" />
+						<input type="text" name="settings[regexp][<?php print $i ?>][regexp]" class="a-regexp" value="<?php print esc_attr($regexp); ?>" />
 					</td>
 					<td>
 						<input type="text" name="settings[regexp][<?php print $i ?>][key]" value="<?php print esc_attr($key); ?>" />
@@ -47,7 +47,7 @@ $admin = get_option('admin_email');
 			<?php for ($j = 0; $j < 5; $j++) { ?>
 				<tr>
 					<td>
-						<input type="text" name="settings[regexp][<?php print $j+$i ?>][regexp]" value="" />
+						<input type="text" name="settings[regexp][<?php print $j+$i ?>][regexp]" class="a-regexp" value="" />
 					</td>
 					<td>
 						<input type="text" name="settings[regexp][<?php print $j+$i ?>][key]" value="" />
@@ -59,6 +59,9 @@ $admin = get_option('admin_email');
 					<input type="submit" name="op" value="<?php print esc_attr__('Save settings', 'wpes'); ?>" class="button-primary action" />
 				</td>
 			</tr>
+			<tr>
+				<td><em> *) You must include the barriers, so start with / and end with /</em></td>
+			</tr>
 			<tr class="header">
 				<th><?php _e('Unmatched subjects', 'wpes'); ?></th>
 			</tr>
@@ -69,12 +72,57 @@ $admin = get_option('admin_email');
 			foreach ($fails as $fail) { ?>
 				<tr>
 					<td>
-						<code><?php print $fail; ?></code>
+						<code class="a-fail"><?php print $fail; ?></code>
 					</td>
 				</tr>
 			<?php } ?>
 			</tbody>
 		</table>
-		<em> *) You must include the barriers, so start with / and end with / and optional modifier</em>
 	</form>
 </div>
+<script>
+	jQuery(document).ready(function(){
+		var t = function(){
+			if (/^\/[\s\S]+\/$/.test( (jQuery(this).val()||"") )) {
+				var that = this;
+				var re = jQuery(that).val();
+				// re matches /asdsadasdasd/, so use substring to get rid of those /-es
+				re = new RegExp(re.substr(1, re.length-2));
+
+				jQuery(".a-fail").each(function(){
+					jQuery(this).toggleClass( 'match', re.test( (jQuery(this).text()||"") ));
+				});
+			}
+			else {
+				jQuery(".a-fail").removeClass('match');
+			}
+		};
+		jQuery(".a-regexp").bind('blur', function(){
+			var val = (jQuery(this).val()||"");
+			if ("" == val) {
+				return jQuery(this).removeClass('error match');
+			}
+			jQuery(this).toggleClass( 'error', ! /^\/[\s\S]+\/$/.test( val )).not('.error').addClass('match');
+		}).bind('focus', function(e){
+			jQuery(".a-fail,.a-regexp").removeClass('match');
+			jQuery(this).removeClass( 'error match' );
+			t.apply(this, [e]);
+		}).bind('keyup', t);
+	});
+</script>
+<style>
+	.a-regexp {
+		width: 300px;
+	}
+	.a-regexp.error {
+		border-color: red;
+		background: #ff9e8b;
+	}
+	.a-regexp.match {
+		border-color: #00a800;
+		background: #97e396;
+	}
+	.a-fail.match {
+		background-color: #80ff8e
+	}
+</style>
